20c51950c9f7cc879fec638758cef04ea28aef87
sanket1998 <sanket1998@outlook.com>
2020-10-28 16:07:47 +1300

    Added server(MongoFB) & finished Add Note

---
+++ server/server.js
@@ -0,0 +1,98 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+//DB Connection Settings and Setup
+const url =
+    "mongodb+srv://dbAdmin:Vp595nRHGcAYO7AB@learningcluster.jwqrf.mongodb.net/notesDB?retryWrites=true&w=majority";
+const connectionProperties = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+};
+
+const noteSchema = new mongoose.Schema({
+    title: String,
+    content: String,
+    date: Date,
+});
+
+const Note = mongoose.model("Note", noteSchema);
+
+//Routing for the App
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/api/addNote", (req, res) => {
+    //Connect to the DB
+    mongoose.connect(url, connectionProperties);
+    //Create a new Note Object
+    const newNote = new Note({
+        title: req.body.title,
+        content: req.body.content,
+        date: new Date(),
+    });
+
+    //finally post it to the database and close connection
+    newNote.save((err) => {
+        //handle any errors along the way
+        if (err) {
+            res.status(err.status).json(err);
+        } else {
+            res.status(200).json("Success");
+        }
+    });
+});
+
+//get all the notes in the DB
+app.get("/api/getAll", (req, res) => {
+    //Connect to the DB
+    mongoose.connect(url, connectionProperties);
+
+    //Fetch all the notes from the DB
+    Note.find({}, (err, foundNotes) => {
+        if (err) {
+            mongoose.connection.close();
+            res.status(err.status).json(err);
+        } else {
+            res.status(200).json({
+                notes: foundNotes,
+                length: foundNotes.length,
+            });
+        }
+    });
+});
+
+//get a specific note based on the ID
+app.get("/api/getNote", (req, res) => {
+    //connect to DB
+    //fetch the desired Note
+    //send the response
+});
+
+//deletes a specific note based on ID
+app.delete("/api/deleteNote", (req, res) => {
+    //connect to DB
+    //delete the required Note
+    //send response
+});
+
+//patches a specific Note based on ID
+app.patch("/api/updateNote", (req, res) => {
+    //connect to DB
+    //create object with modidified properties
+    //update the database
+    //send response
+});
+
+app.get("/test", (req, res) => {
+    const message = "Working correctly!";
+    res.json(message);
+});
+
+app.listen(process.env.port || 5000, () => {
+    console.log("Server started on port 5000");
+});
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,45 +1,64 @@
 import React, { useState } from "react";
+import axios from "axios";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
+function useGetAllHook() {
+    const [notes, setNote] = useState([]);
+    //fetches all the notes to render
+    axios
+        .get("/api/getAll")
+        .then((res) => {
+            if (res.status === 200) {
+                setNote(res.data.notes);
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    return notes;
+}
+
 function App() {
-  const [notes, setNote] = useState([]);
+    const notes = useGetAllHook();
 
-  function handleAddNote(note) {
-    setNote((prevNotes) => {
-      return [...prevNotes, note];
-    });
-  }
+    function handleAddNote(newNote) {
+        axios
+            .post("/api/addNote", newNote)
+            .then((res) => {})
+            .catch((res) => {
+                console.log(res);
+            });
+    }
 
-  function deleteNote(id) {
-    setNote((prevNotes) => {
-      return prevNotes.filter((note, index) => {
-        return index !== id;
-      });
-    });
-  }
+    function deleteNote(id) {
+        // setNote((prevNotes) => {
+        //     return prevNotes.filter((note, index) => {
+        //         return index !== id;
+        //     });
+        // });
+    }
 
-  return (
-    <div>
-      <Header />
-      <CreateArea addNote={handleAddNote} />
-      {notes.map((note, index) => {
-        return (
-          <Note
-            key={index}
-            id={index}
-            onClick={deleteNote}
-            title={note.title}
-            content={note.content}
-          />
-        );
-      })}
-
-      <Footer />
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            <CreateArea addNote={handleAddNote} />
+            {notes.map((note, index) => {
+                return (
+                    <Note
+                        key={index}
+                        id={index}
+                        onClick={deleteNote}
+                        title={note.title}
+                        content={note.content}
+                    />
+                );
+            })}
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
